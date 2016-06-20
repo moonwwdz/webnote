21393158d74aea80ac6170b09650ae8538b28285
moonwwdz <wangy3483@gmail.com>
2016-06-20 17:18:20 +0800

    添加注册登录模板

--- main.go
+++ main.go
@@ -2,10 +2,17 @@ package main
 
 import (
 	_ "webnote/routers"
+
 	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/orm"
 )
 
-func main() {
-	beego.Run()
+func init() {
+	orm.RegisterDriver("mysql", orm.DRMySQL)
+	orm.RegisterDataBase("default", "mysql", "root:root@/orm_test?charset=utf8")
 }
 
+func main() {
+	beego.SetStaticPath("/static", "static")
+	beego.Run()
+}

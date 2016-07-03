335795d572654e14e74349cc100ce6c13563aa94
moonwwdz <wangy3483@gmail.com>
2016-07-03 23:44:00 +0800

    优化一下新增中关联tag

--- models/notebook.go
+++ models/notebook.go
@@ -1,6 +1,7 @@
 package models
 
 import (
+	"strings"
 	"time"
 
 	"github.com/astaxie/beego"
@@ -26,24 +27,25 @@ func init() {
 func AddNote(note *Notebook, tags *[]string) bool {
 	var tagObjs []*Tag
 	o := orm.NewOrm()
-	beego.Debug(tags)
-	for _, tag := range *tags {
-		tagObj := Tag{}
-		tagObj.Tag = tag
-		tagObj.Notebook = []*Notebook{note}
-		if err := o.QueryTable("tag").Filter("Tag", tag).One(&tagObj); tagObj.Id != 0 {
-			beego.Debug(err)
-			tagObjs = append(tagObjs, &tagObj)
-		} else {
-			_, err := o.Insert(&tagObj)
-			if err == nil {
-				tagObjs = append(tagObjs, &tagObj)
-			} else {
-				beego.Debug(err)
-			}
-		}
-		beego.Debug(tagObj)
-	}
+	// beego.Debug(tags)
+	// for _, tag := range *tags {
+	// 	tagObj := Tag{}
+	// 	tagObj.Tag = tag
+	// 	tagObj.Notebook = []*Notebook{note}
+	// 	if err := o.QueryTable("tag").Filter("Tag", tag).One(&tagObj); tagObj.Id != 0 {
+	// 		beego.Debug(err)
+	// 		tagObjs = append(tagObjs, &tagObj)
+	// 	} else {
+	// 		_, err := o.Insert(&tagObj)
+	// 		if err == nil {
+	// 			tagObjs = append(tagObjs, &tagObj)
+	// 		} else {
+	// 			beego.Debug(err)
+	// 		}
+	// 	}
+	// 	beego.Debug(tagObj)
+	// }
+	tagObjs = GetOrCreateTags(tags)
 	note.UpdateTime = time.Time{}
 	note.Tag = tagObjs
 	if _, nerr := o.Insert(note); nerr != nil {
@@ -58,12 +60,39 @@ func AddNote(note *Notebook, tags *[]string) bool {
 	return true
 }
 
+//取一条笔记
+func GetNote(id int) *Notebook {
+	note := new(Notebook)
+	err := orm.NewOrm().QueryTable("notebook").Filter("Id", id).One(note)
+	if err != nil {
+		beego.Debug(err)
+	}
+	return note
+}
+
+//取一条笔记里的tag
+func GetNoteTags(note *Notebook) string {
+	var title []string
+
+	if _, terr := orm.NewOrm().LoadRelated(note, "Tag"); terr != nil {
+		beego.Debug(terr)
+	}
+	for _, t := range note.Tag {
+		title = append(title, t.Tag)
+	}
+	return strings.Join(title, ",")
+}
+
 //删除一条笔记
 func DelNote(id int) bool {
 	return true
 }
 
 //更新一条笔记
-func UpdateNote(id int) bool {
+func UpdateNote(note *Notebook) bool {
+	if _, err := orm.NewOrm().Update(note); err != nil {
+		beego.Debug(err)
+		return false
+	}
 	return true
 }
--- models/tag.go
+++ models/tag.go
@@ -1,6 +1,9 @@
 package models
 
-import "github.com/astaxie/beego/orm"
+import (
+	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/orm"
+)
 
 type Tag struct {
 	Id       int
@@ -22,4 +25,24 @@ func AddTag(tag string) bool {
 func GetTagList() map[int]Tag {
 	tags := make(map[int]Tag)
 	return tags
+}
+
+//根据切片中的字符串，取得数据库中的对象，如果没有，新加一个对应的对象
+//返回对象的切片
+func GetOrCreateTags(tags *[]string) []*Tag {
+	var tagObjs []*Tag
+	o := orm.NewOrm()
+
+	for _, tag := range *tags {
+		tagObj := Tag{Tag: tag}
+		if err := o.QueryTable("tag").Filter("Tag__Tag__Tag", tag).One(&tagObj); err != nil {
+			beego.Debug(err)
+			if _, err := o.Insert(&tagObj); err != nil {
+				beego.Debug(err)
+			}
+		}
+		tagObjs = append(tagObjs, &tagObj)
+	}
+	beego.Debug(tagObjs)
+	return tagObjs
 }
